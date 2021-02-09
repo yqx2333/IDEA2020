f1f03b63699fef6e6eb350719011038cbcacd0b5
YuQingXi <840174265@qq.com>
2021-02-09 11:41:27 +0800

    创建测试

---
+++ src/com/yqx/Hello.java
@@ -0,0 +1,10 @@
+package com.yqx;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println( "HelloWorld" );
+    }
+
+}
---
+++ src/com/yqx/Main.java
@@ -0,0 +1,9 @@
+package com.yqx;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(123);
+    }
+}
